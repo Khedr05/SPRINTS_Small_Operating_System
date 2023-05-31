3cd9bdac9caccd444de9243c9410def987d6fed8
momen <momenhassan@gmail.com>
2023-06-01 00:59:16 +0200

    push

---
+++ 00_Project/sos/MCAL/dio/dio_cfg.h
@@ -0,0 +1,17 @@
+#ifndef DIO_CONFIG_H
+#define DIO_CONFIG_H
+
+
+// Pin modes
+#define DIOMODE_INPUT     0
+#define DIOMODE_OUTPUT    1
+
+// Input/output settings
+#define DIOOUTPUT_LOW      0
+#define DIOOUTPUT_HIGH     1
+
+// Pull-up resistor configurations
+#define DIOPULLUP_DISABLED 0
+#define DIOPULLUP_ENABLED  1
+
+#endif /* DIO_CONFIG_H */
